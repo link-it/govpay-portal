f3590f40b1279c9070b1937977b19c7c1ef87dc9
simo-git <>
2021-07-29 12:23:41 +0200

    Issue #53 - Revisione portale 4.0 - Bugfix flickering e CSS menu

--- src/app/elements/field-group/field-group.component.ts
+++ src/app/elements/field-group/field-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, HostBinding, AfterContentChecked, ViewChild, ElementRef, NgZone, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, HostBinding, AfterViewInit, ViewChild, ElementRef, NgZone, OnDestroy, HostListener } from '@angular/core';
 import { BehaviorSubject, Subscription } from 'rxjs/index';
 
 @Component({
@@ -6,7 +6,10 @@ import { BehaviorSubject, Subscription } from 'rxjs/index';
   templateUrl: './field-group.component.html',
   styleUrls: ['./field-group.component.scss']
 })
-export class FieldGroupComponent implements OnInit, AfterContentChecked, OnDestroy {
+export class FieldGroupComponent implements OnInit, AfterViewInit, OnDestroy {
+  @HostListener('window:resize') _resizing() {
+    this.__hoverable();
+  }
   @HostBinding('class.hovered') hoverable: boolean = false;
   @ViewChild('lf', { read: ElementRef }) _lf: ElementRef;
   @Input() label: string = '';
@@ -27,13 +30,12 @@ export class FieldGroupComponent implements OnInit, AfterContentChecked, OnDestr
   @Output() iconClickEvt: EventEmitter<any> = new EventEmitter(null);
 
   _sub: Subscription;
+  _breakpointSize: number = 768;
 
   constructor(private zone: NgZone) {
     this._sub = Notifier.subscribe((value) => {
       if (value) {
-        this.zone.run(() => {
-          this.__hoverable();
-        });
+        this.__hoverable();
       }
     });
   }
@@ -48,15 +50,32 @@ export class FieldGroupComponent implements OnInit, AfterContentChecked, OnDestr
     }
   }
 
-  ngAfterContentChecked() {
-    this.__hoverable();
+  ngAfterViewInit() {
+    switch (this.bp) {
+      case 'sm':
+        this._breakpointSize = 576;
+        break;
+      case 'lg':
+        this._breakpointSize = 992;
+        break;
+      default:
+        this._breakpointSize = 768;
+    }
+    setTimeout(() => {
+      this.__hoverable();
+    });
   }
 
   __hoverable() {
-    this.hoverable = false;
-    if (this._lf) {
-      this.hoverable = (this._lf.nativeElement.offsetWidth < this._lf.nativeElement.scrollWidth);
-    }
+    this.zone.run(() => {
+      if (window.innerWidth >= this._breakpointSize) {
+        if (this._lf) {
+          this.hoverable = (this._lf.nativeElement.offsetWidth < this._lf.nativeElement.scrollWidth);
+        }
+      } else {
+        this.hoverable = false;
+      }
+    });
   }
 
   _onIconDownload() {
