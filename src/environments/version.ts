728d96bdfa43f3af4b0b39a2b677db48977ccf48
Giuliano Pintori <pintori@link.it>
2026-06-29 12:46:42 +0200

    Fix versione.

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -3,7 +3,7 @@
  *
  * FILE AUTO-GENERATO - NON MODIFICARE MANUALMENTE
  * Generato da: scripts/generate-version.js
- * Data generazione: 2026-06-26T16:20:57.672Z
+ * Data generazione: 2026-06-29T09:55:42.580Z
  */
 
 export interface VersionInfo {
@@ -19,13 +19,13 @@ export interface VersionInfo {
 
 export const VERSION: VersionInfo = {
   version: '5.2.1',
-  gitHash: 'ee61e54',
-  gitHashFull: 'ee61e54658dae4b818c71f59167d09cfbcfdd014',
+  gitHash: 'ed03dee',
+  gitHashFull: 'ed03dee2cd4423acb1d350042ac8670175c3e9cc',
   gitBranch: 'master',
-  gitTag: '5.2.0',
+  gitTag: '5.2.1',
   gitDirty: true,
-  buildDate: '2026-06-26T16:20:57.672Z',
-  buildTimestamp: 1782490857673
+  buildDate: '2026-06-29T09:55:42.580Z',
+  buildTimestamp: 1782726942581
 };
 
 /**
